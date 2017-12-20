438b4b1e4968e59321e0fa8ffc88b347b04564ec
Joao <joao.paulo@zup.com.br>
2017-12-20 21:27:22 -0200

    Correcao para erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './containers/home/home.component';
-import { ExampleComponentComponent } from './components/example-component/example-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ExampleComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
