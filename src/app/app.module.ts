f3eeac5dc3f5faa1ad772aa295bd102a409c56e8
Joao <joao.paulo@zup.com.br>
2018-01-26 21:10:41 -0200

    Correco para estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
+import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/http-service/interceptor/http.interceptor.ts
+++ src/app/core/http-service/interceptor/http.interceptor.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/finally';
 
-import { HeadersHttp } from '@core/http-service/header/http.header';
+import { HeadersHttp } from '@interceptor/http-header/http.header';
 import { ENVIRONMENT } from '@env/environment';
 
 const API_URL = ENVIRONMENT.baseUrl;
